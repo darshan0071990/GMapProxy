3b087c010fb5863aeff8dfde54d07dc1b270d7fb
Darshan Hingu <hingu.darshan@gmail.com>
2018-02-03 22:19:36 +0100

    Created Proxy to Retrieve Map Tiles from Google API

---
+++ src/Proxy.java
@@ -0,0 +1,105 @@
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@javax.servlet.annotation.WebServlet(name = "Proxy")
+public class Proxy extends javax.servlet.http.HttpServlet {
+    private static final int BUFFER_SIZE = 4096;
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws IOException {
+        String center = request.getParameter("center");
+        String zoom = request.getParameter("zoom");
+
+        String fileName = center +"_" +zoom+".png";
+        String directoryName = getServletContext().getRealPath("/") + "download"+File.separator;
+
+        File mapTile = fileCreation(directoryName, fileName);
+
+        if(mapTile.exists()){
+            System.out.println("File Exists");
+        }else {
+            System.out.println("File needs to Download");
+            downloadTile(center, zoom, mapTile);
+        }
+
+        response.setContentType("image/png");
+        ServletOutputStream out= response.getOutputStream();
+        FileInputStream fin = new FileInputStream(mapTile);
+
+        BufferedInputStream bin = new BufferedInputStream(fin);
+        BufferedOutputStream bout = new BufferedOutputStream(out);
+        int ch =0;
+        while((ch=bin.read())!=-1)
+        {
+            bout.write(ch);
+        }
+
+        bin.close();
+        fin.close();
+        bout.close();
+        out.close();
+    }
+
+    private void downloadTile(String center, String zoom, File fileName) {
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + center + "&zoom=" + zoom + "&size=512x512&key=XXXXXXXXXX");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+
+            int responseCode = conn.getResponseCode();
+
+            // always check HTTP response code first
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // opens input stream from the HTTP connection
+                InputStream inputStream = conn.getInputStream();
+                writeFile(inputStream,fileName);
+
+                System.out.println("File downloaded");
+            } else {
+                System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+            }
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File fileCreation(String directoryName, String fileName) {
+        File directory = new File(directoryName);
+        if (!directory.exists()){
+            directory.mkdir();
+        }
+        File imageDest = new File(directory.getAbsoluteFile() + "/" + fileName);
+
+        return imageDest;
+    }
+
+    public void writeFile(InputStream inputStream, File fileName){
+
+        try{
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            int bytesRead = -1;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            inputStream.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: darshan
+  Date: 03/02/18
+  Time: 2:25 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <p>To invoke the java servlet click <a href="Proxy">here</a></p>
+  </body>
+</html>
